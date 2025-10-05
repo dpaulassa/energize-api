ab806e81ad367d83c74185c7a8951e4082f7a006
Maysa Angélica <maysa.dpaulas@gmail.com>
2025-10-05 19:04:25 -0300

    Initial commit: Add Energize API project and CI workflow

---
+++ src/main/java/br/com/fiap/energizeapi/EnergizeapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.energizeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergizeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergizeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/energizeapi/EnergizeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.energizeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergizeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
